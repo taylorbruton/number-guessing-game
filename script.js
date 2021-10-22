69284328339cb010a81e37dd86a1233c48567d9a
taylorbruton <89506980+taylorbruton@users.noreply.github.com>
2021-10-21 23:56:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-<script>
       let randomNumber = Math.floor(Math.random() * 100) + 1;
+
       const guesses = document.querySelector('.guesses');
       const lastResult = document.querySelector('.lastResult');
       const lowOrHi = document.querySelector('.lowOrHi');
       const guessSubmit = document.querySelector('.guessSubmit');
       const guessField = document.querySelector('.guessField');
+     
       let guessCount = 1;
       let resetButton;
 
@@ -66,5 +67,3 @@
         lastResult.style.backgroundColor = 'white';
         randomNumber = Math.floor(Math.random() * 100) + 1;
       }
-   
-</script> 
